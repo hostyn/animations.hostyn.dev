28b3539c59b6487653d90caa6c0710bc3abf3b32
Hostyn <ruben@martinezhostyn.com>
2023-10-01 18:13:50 +0100

    feat: :wheelchair: Improve font contrast in ColorPicker ui component
    Imporove contrast by setting the font color depending on the value

--- src/components/ui/ColorPicker.tsx
+++ src/components/ui/ColorPicker.tsx
@@ -11,11 +11,18 @@ export default function ColorPicker({
   style,
   onChange,
 }: ColorPickerProps) {
+  const colorValue =
+    parseInt(value.replaceAll("#", "").slice(0, 2), 16) +
+    parseInt(value.replaceAll("#", "").slice(2, 4), 16) +
+    parseInt(value.replaceAll("#", "").slice(4, 6), 16);
+
+  console.log(colorValue);
+
   return (
     <label
       style={style}
       className={css({
-        color: "black",
+        color: colorValue > 382 ? "black" : "white",
         fontWeight: 600,
         width: "100%",
         height: "80px",
