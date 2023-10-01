a29a0f80e8e15e0a87f1698f2d59df7c63527ef6
Hostyn <ruben@martinezhostyn.com>
2023-10-01 18:38:42 +0100

    fix: :bug: Fix font import not working in production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import "@styles/index.css";
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main>
       <Component {...pageProps} />
     </main>
   );
