246ef44350fefb539a24210a4f4b22c2bd6ee9b1
Hostyn <ruben@martinezhostyn.com>
2023-10-02 11:20:35 +0100

    feat: :fire: Remove unused keyframe

--- panda.config.ts
+++ panda.config.ts
@@ -39,22 +39,6 @@ export default defineConfig({
           },
         },
       },
-
-      keyframes: {
-        "text-popup": {
-          "0%": {
-            transform: "translateY(0)",
-            transformOrigin: "50% 50%",
-            textShadow: "none",
-          },
-          "100%": {
-            transform: "translateY(-50px)",
-            transformOrigin: "50% 50%",
-            textShadow:
-              "0 1px 0 #cccccc, 0 2px 0 #cccccc, 0 3px 0 #cccccc, 0 4px 0 #cccccc, 0 5px 0 #cccccc, 0 6px 0 #cccccc, 0 7px 0 #cccccc, 0 8px 0 #cccccc, 0 9px 0 #cccccc, 0 50px 30px rgba(0, 0, 0, 0.3)",
-          },
-        },
-      },
     },
   },
 
